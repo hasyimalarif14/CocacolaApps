c0ad5419f17119c0ea6385914e971f4d72aa7585
hasyimalarif14 <130950089+hasyimalarif14@users.noreply.github.com>
2023-05-02 13:58:42 +0700

    First Update

---
+++ src/cacocola/koneksi.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cacocola;
+
+import java.awt.HeadlessException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author affad
+ */
+
+public class koneksi {
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+        // TODO code application logic here
+        private static Connection akses;
+      public static Connection akses()throws Exception{
+            try{
+                String url ="jdbc:mysql://localhost/warehouse";
+                String user = "root";
+                String pass ="";
+                
+                DriverManager.getConnection(url,"root","");
+                akses = DriverManager.getConnection(url, user, pass);
+                
+            } catch (HeadlessException | SQLException e){
+                System.out.println("koneksi gagal"+ e.getMessage());
+        }
+            return akses;
+    }
+    
+}
